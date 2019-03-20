f8e962108588d2536a55cd894a6792871260cf60
jerry <email@jerry.com>
2019-03-20 12:01:28 +0800

    '回复模板,回复用户消息的6种消息'

--- models/reply.js
+++ models/reply.js
@@ -1,17 +1,14 @@
 // 处理中间件
 const sha1 = require('sha1');
 const {xmlMsg,parseData,formatJsData} = require('./Data.js');
+const template = require('./template');
 
 module.exports =()=>{
-   // app.use();
+
     return   async(req,res)=>{
 
         const {signature,echostr,timestamp,nonce,}=req.query;
         const token = 'atguiguwechat721';
-
-        /*const Arr = [token,timestamp,nonce];
-        const Str = Arr.sort().join('');
-        const sha1Str = sha1(Str);*/
 
         const sha1Str = sha1([token,timestamp,nonce].sort().join(''));
 
@@ -30,56 +27,43 @@ module.exports =()=>{
                 return;
             }
 
-            /*const xmlData = await new Promise((resolve,reject)=>{
-               let xmlData = '';
-
-                req.on('data',data=>{
-                    xmlData += data.toString();
-                })
-                    .on('end',()=>{
-                        resolve(xmlData)
-                    })
-             });*/
             //获取用户发过来的信息
             const xmlData = await xmlMsg(req);
 
-            /*  let jsData = null;
-              parseString(xmlData,{trim:true},(err,result)=>{
-                  if(!err){
-                     jsData = result;
-                  }else{
-                      jsData ={};
-                  }
-              })*/
-            //将xmlData 解析 jsData
             const jsData = parseData(xmlData);
 
-            /* let userData = {};
-             for(let key in xml){
-                 const value = xml[key];
-                 userData[key] = value[0];
-             }*/
-            //格式化jsData
             const userData =formatJsData(jsData);
 
+            const options ={
+                toUserName: userData.FromUserName,
+                fromUserName: userData.ToUserName,
+                createTime: Date.now(),
+                type: 'text',
+                content: '如果你是奇葩,你就扣1'
+            };
+
+
             //创建自动回复内容
-            let content = '如果你是奇葩,你就扣1';
+           // let content = '如果你是奇葩,你就扣1';
 
-            if(userData.Content === '1'){
-                content = '恭喜你';
-            }else if (userData.Content.indexOf('2') !== -1) {
-                content = '你很棒!';
-            }
+           if(userData.MsgType === 'text'){
+               if(userData.Content === '1'){
+                   options.content = '恭喜你';
+                   //options.type = 'text';
+               }else if (userData.Content.indexOf('2') !== -1) {
+                   options.content = '你很棒!';
+                  // options.type = 'text';
+               }
+           }else if(userData.MsgType === 'image'){
 
-            let MsgData = `<xml>
-        <ToUserName><![CDATA[${userData.FromUserName}]]></ToUserName>
-        <FromUserName><![CDATA[${userData.ToUserName}]]></FromUserName>
-        <CreateTime>${Date.now()}</CreateTime>
-        <MsgType><![CDATA[text]]></MsgType>
-        <Content><![CDATA[${content}]]></Content>
-        </xml>`
+               options.mediaId = userData.MediaId;
+               options.type = 'image';
+           }
 
-            // console.log(MsgData);
+            const MsgData = template(options);
+
+             console.log(MsgData);
+
             res.send(MsgData)
 
         }else{
---
+++ models/template.js
@@ -0,0 +1,69 @@
+//定义回复用户消息的6种模板模块
+
+module.exports =(options)=>{
+    let MsgData = `<xml>
+        <ToUserName><![CDATA[${options.toUserName}]]></ToUserName>
+        <FromUserName><![CDATA[${options.fromUserName}]]></FromUserName>
+        <CreateTime>${options.createTime}</CreateTime>
+         <MsgType><![CDATA[${options.type}]]></MsgType>`
+
+    if(options.type === 'text'){
+        MsgData += `<Content><![CDATA[${options.content}]]></Content>`
+
+    }else if(options.type === 'image'){
+
+        MsgData +=`<Image>
+        <MediaId><![CDATA[${options.mediaId}]]></MediaId>
+        </Image>`
+    }else if(options.type === 'voice'){
+        MsgData +=`<Voice>
+    <MediaId><![CDATA[${options.mediaId}]]></MediaId>
+  </Voice>`
+    }else if(options.type === 'video'){
+        MsgData +=`<Video>
+    <MediaId><![CDATA[${options.mediaId}]]></MediaId>
+    <Title><![CDATA[${options.title}]]></Title>
+    <Description><![CDATA[${options.description}]]></Description>
+  </Video>`
+    }else if(options.type === 'music'){
+        MsgData +=`<Music>
+    <Title><![CDATA[${options.title}]]></Title>
+    <Description><![CDATA[${options.description}]]></Description>
+    <MusicUrl><![CDATA[${options.musicUrl}]]></MusicUrl>
+    <HQMusicUrl><![CDATA[${options.hqMusicUrl}]]></HQMusicUrl>
+    <ThumbMediaId><![CDATA[${options.mediaId}]]></ThumbMediaId>
+  </Music>`
+    }else if(options.type === 'news'){
+       /* MsgData +=`
+   <ArticleCount>1</ArticleCount>
+   <Articles>
+    <item>
+      <Title><![CDATA[title1]]></Title>
+      <Description><![CDATA[description1]]></Description>
+      <PicUrl><![CDATA[picurl]]></PicUrl>
+      <Url><![CDATA[url]]></Url>
+    </item>
+  </Articles>`*/
+        MsgData +=` <ArticleCount>${options.content.length}</ArticleCount>
+<Articles>`
+ /*       MsgData += options.content.reduce((pre,curr)=> {
+            return pre +` <item>
+           <Title><![CDATA[${options.title}]]></Title>
+           <Description><![CDATA[${options.description}]]></Description>
+           <PicUrl><![CDATA[${options.picUrl}]]></PicUrl>
+           <Url><![CDATA[${options.Url}]]></Url>
+           </item>`
+},'');*/
+        MsgData += options.content.reduce((pre,curr)=> {
+            return pre +` <item>
+           <Title><![CDATA[${curr.title}]]></Title>
+           <Description><![CDATA[${curr.description}]]></Description>
+           <PicUrl><![CDATA[${curr.picUrl}]]></PicUrl>
+           <Url><![CDATA[${curr.Url}]]></Url>
+           </item>`
+        },'')
+        MsgData += `</Articles>`;
+    }
+    MsgData +=`</xml>`;
+return MsgData;
+}
