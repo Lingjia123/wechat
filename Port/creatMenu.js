7bd8f37b1d2be4f80252a9d2fec9111048808c0c
jerry <email@jerry.com>
2019-03-21 16:52:37 +0800

    '新增用户标签管理'

--- Port/creatMenu.js
+++ Port/creatMenu.js
@@ -79,7 +79,7 @@ async function creatMenu(){
     //函数要return 才会有输出
     return result;
 }
-//creatMenu()
+
 async function DeleteMenu(){
     //获取accesstoken
     const {access_token} = await fetchAccessToken();
@@ -88,14 +88,49 @@ async function DeleteMenu(){
     const result = rp({method:'GET',url,json:true,},);
     return result;
 }
+
+//1. 创建标签
+async function creatTag(name){
+    //获取accesstoken
+    const {access_token} = await fetchAccessToken();
+    const url = `https://api.weixin.qq.com/cgi-bin/tags/create?access_token=${access_token}`;
+    return await rp({method:'POST',url,json:true,body:{tag:{name}}})
+}
+//2. 获取公众号已创建的标签
+async function getcreatTag(){
+    //获取accesstoken
+    const {access_token} = await fetchAccessToken();
+    const url = `https://api.weixin.qq.com/cgi-bin/tags/get?access_token=${access_token}`;
+    return await rp({method:'GET',url,json:true,});
+}
+//5. 获取标签下粉丝列表
+async function getTagList(tagid,next_openid=''){
+    //获取accesstoken
+    const {access_token} = await fetchAccessToken();
+    const url = `https://api.weixin.qq.com/cgi-bin/user/tag/get?access_token=${access_token}`;
+    return await rp({method:'POST',url,json:true,body:{tagid,next_openid}});
+}
+//1. 批量为用户打标签
+async function batchUserTag(openid_list,tagid){
+    //获取accesstoken
+    const {access_token} = await fetchAccessToken();
+    const url = `https://api.weixin.qq.com/cgi-bin/tags/members/batchtagging?access_token=${access_token}`;
+    return await rp({method:'POST',url,json:true,body:{openid_list,tagid}});
+}
+
 //立即执行函数
 (async()=>{
-   let result = await DeleteMenu();
-    console.log(result);
-    result = await creatMenu();
-    console.log(result);
-})()
+   // let result = await DeleteMenu();
+   //  console.log(result);
+   //  result = await creatMenu();
+   //  console.log(result);
+   //  let result1 = await creatTag('小佳1');
+   //  console.log(result1); //{ tag: { id: 100, name: '小佳' } }
+   let result2 = await getcreatTag();
+    console.log(result2);
+   let result3 = await getTagList(100);
+    console.log(result3);
+    let result4 =await batchUserTag(['oUkfA598b3bF1ILC1mL4wBwEQbnc'],100);
+    console.log(result4);
+})();
 
-
-/*{ errcode: 0, errmsg: 'ok' }
-{ errcode: 0, errmsg: 'ok' }*/
